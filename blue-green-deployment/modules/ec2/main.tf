0590872cb9ff81cb258d3005a314a7066b372ad5
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-03-06 20:19:40 +0530

    Add files via upload

---
+++ blue-green-deployment/modules/ec2/main.tf
@@ -0,0 +1,84 @@
+
+
+resource "aws_instance" "blue" {
+  ami           = "ami-05b10e08d247fb927"
+  instance_type = var.instance_type
+  key_name      = "blue-green-key-pair"  # Use the existing key in AWS
+  subnet_id     = var.subnet_id
+  security_groups = [var.ec2_security_group_id]
+
+  tags = {
+    Name = "Blue-Instance"
+  }
+
+  provisioner "file" {
+    source      = "C:/Users/Tanishq.Parab/Desktop/Projects/blue-green-deployment/modules/ec2/scripts/install_dependencies.sh"
+    destination = "/home/ec2-user/install_dependencies.sh"
+  }
+
+  provisioner "file" {
+    source      = "C:/Users/Tanishq.Parab/Desktop/Projects/blue-green-deployment/modules/ec2/scripts/app.py"
+    destination = "/home/ec2-user/app.py"
+  }
+
+  provisioner "file" {
+    source      = "C:/Users/Tanishq.Parab/Desktop/Projects/blue-green-deployment/Jenkinsfile"
+    destination = "/home/ec2-user/Jenkinsfile"
+  }
+
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ec2-user/install_dependencies.sh",
+      "sudo /home/ec2-user/install_dependencies.sh"
+    ]
+  }
+
+connection {
+  type        = "ssh"
+  user        = "ec2-user"  # Change if using a different AMI
+  private_key = file("blue-green-key.pem")
+  host        = self.public_ip
+}
+}
+
+resource "aws_instance" "green" {
+  ami           = "ami-05b10e08d247fb927"
+  instance_type = var.instance_type
+  key_name      = "blue-green-key-pair"  # Use the existing key in AWS
+  subnet_id     = var.subnet_id
+  security_groups = [var.ec2_security_group_id]
+  
+  tags = {
+    Name = "Green-Instance"
+  }
+
+    provisioner "file" {
+    source      = "C:/Users/Tanishq.Parab/Desktop/Projects/blue-green-deployment/modules/ec2/scripts/install_dependencies.sh"
+    destination = "/home/ec2-user/install_dependencies.sh"
+  }
+
+  provisioner "file" {
+    source      = "C:/Users/Tanishq.Parab/Desktop/Projects/blue-green-deployment/modules/ec2/scripts/app.py"
+    destination = "/home/ec2-user/app.py"
+  }
+
+  provisioner "file" {
+    source      = "C:/Users/Tanishq.Parab/Desktop/Projects/blue-green-deployment/Jenkinsfile"
+    destination = "/home/ec2-user/Jenkinsfile"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ec2-user/install_dependencies.sh",
+      "sudo /home/ec2-user/install_dependencies.sh"
+    ]
+  }
+
+ connection {
+  type        = "ssh"
+  user        = "ec2-user"  # Change if using a different AMI
+  private_key = file("blue-green-key.pem")
+  host        = self.public_ip
+}
+}
