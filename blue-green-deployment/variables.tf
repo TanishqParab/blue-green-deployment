39981ed6b944726b808cbd0db05b730d51a35e16
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-03-11 13:34:05 +0530

    Update variables.tf

--- blue-green-deployment/variables.tf
+++ blue-green-deployment/variables.tf
@@ -28,6 +28,7 @@ variable "private_key_path" {
 variable "public_key_path" {
   description = "Path to the public key file"
   type        = string
+  default     = "/var/lib/jenkins/.ssh/blue-green-key.pub"
 }
 
 
