589dd6f304d3f1305518c5dc037f99059c781dc7
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-03-12 10:25:47 +0530

    Update outputs.tf

--- blue-green-deployment/modules/ec2/outputs.tf
+++ blue-green-deployment/modules/ec2/outputs.tf
@@ -5,11 +5,3 @@ output "blue_instance_ip" {
 output "green_instance_ip" {
   value = aws_instance.green.public_ip
 }
-
-output "blue_instance_id" {
-  value = aws_instance.blue.id
-}
-
-output "green_instance_id" {
-  value = aws_instance.green.id
-}
