8e435f8221bd1c4548d1cd4ab1d6170b971d116e
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-03-19 20:47:54 +0530

    Update main.tf

--- blue-green-deployment/modules/alb/main.tf
+++ blue-green-deployment/modules/alb/main.tf
@@ -6,12 +6,19 @@ resource "aws_lb" "main" {
   subnets            = var.subnet_ids  # Now it will receive a list of subnets
 }
 
-
 resource "aws_lb_target_group" "blue" {
   name     = "blue-tg"
   port     = 5000
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "instance"  # Ensuring it targets EC2 instances
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
 }
 
 resource "aws_lb_target_group" "green" {
@@ -19,4 +26,23 @@ resource "aws_lb_target_group" "green" {
   port     = 5000
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "instance"  # Ensuring it targets EC2 instances
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+}
+
+# ✅ Corrected Listener Resource
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn  # ✅ Corrected Reference
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.blue.arn  # ✅ Corrected Reference
+  }
 }
