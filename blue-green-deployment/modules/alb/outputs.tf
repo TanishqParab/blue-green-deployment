4f87a4cde66d410af303dacbb57704e3ba8ab4bf
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-03-20 10:03:56 +0530

    Update outputs.tf

--- blue-green-deployment/modules/alb/outputs.tf
+++ blue-green-deployment/modules/alb/outputs.tf
@@ -9,3 +9,17 @@ output "target_group_arn_blue" {
 output "target_group_arn_green" {
   value = aws_lb_target_group.green.arn
 }
+
+output "blue_id" {
+  description = "Blue target group attachment ID"
+  value       = aws_lb_target_group.blue.id
+}
+
+output "green_id" {
+  description = "Green target group attachment ID"
+  value       = aws_lb_target_group.green.id
+}
+output "alb_arn" {
+  description = "ARN of the ALB"
+  value       = aws_lb.main.arn
+}
