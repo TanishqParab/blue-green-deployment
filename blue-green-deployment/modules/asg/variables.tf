50c01f4a417ac0b8375efc97d8fcb8762a78b3d2
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-03-24 15:16:51 +0530

    Update variables.tf

--- blue-green-deployment/modules/asg/variables.tf
+++ blue-green-deployment/modules/asg/variables.tf
@@ -8,10 +8,12 @@ variable "subnet_ids" {
   type        = list(string)
 }
 
+/*
 variable "alb_target_group_arn" {
   description = "The ARN of the ALB target group"
   type        = string
-}
+}*/
+
 
 variable "security_group_id" {
   description = "The ID of the security group"
