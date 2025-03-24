481d0c2f14d992451e0ee1a931b6339db126733b
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-03-24 15:52:06 +0530

    Update outputs.tf

--- blue-green-deployment/modules/asg/outputs.tf
+++ blue-green-deployment/modules/asg/outputs.tf
@@ -1,3 +1,18 @@
 output "asg_id" {
   value = aws_autoscaling_group.blue_green_asg
 }
+
+output "asg_name" {
+  description = "The name of the Auto Scaling Group"
+  value       = aws_autoscaling_group.blue_green_asg.name
+}
+
+output "blue_target_group_arn" {
+  description = "Target group ARN for Blue instances"
+  value       = var.blue_target_group_arn
+}
+
+output "green_target_group_arn" {
+  description = "Target group ARN for Green instances"
+  value       = var.green_target_group_arn
+}
