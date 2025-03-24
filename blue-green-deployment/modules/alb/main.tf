a1047ff542852dfbab88682e14afae8773b0de6a
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-03-24 13:45:04 +0530

    Update main.tf

--- blue-green-deployment/modules/alb/main.tf
+++ blue-green-deployment/modules/alb/main.tf
@@ -20,3 +20,16 @@ resource "aws_lb_target_group" "green" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
+
+# Add ALB Listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.blue.arn
+  }
+}
+
