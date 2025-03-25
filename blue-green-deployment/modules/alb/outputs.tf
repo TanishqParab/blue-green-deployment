5506e4855da5386c483a7e25e88b09dec6977b9d
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-03-25 10:30:04 +0530

    Update outputs.tf

--- blue-green-deployment/modules/alb/outputs.tf
+++ blue-green-deployment/modules/alb/outputs.tf
@@ -2,11 +2,11 @@ output "alb_dns_name" {
   value = aws_lb.main.dns_name
 }
 
-output "target_group_arn_blue" {
+output "blue_target_group_arn" {
   value = aws_lb_target_group.blue.arn
 }
 
-output "target_group_arn_green" {
+output "green_target_group_arn" {
   value = aws_lb_target_group.green.arn
 }
 
