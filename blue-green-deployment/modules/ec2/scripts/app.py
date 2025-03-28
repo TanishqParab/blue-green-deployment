e09485a5a893e092b9769d165c365fe432891a3b
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-03-28 18:41:06 +0530

    Update app.py

--- blue-green-deployment/modules/ec2/scripts/app.py
+++ blue-green-deployment/modules/ec2/scripts/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Blue-Green Deployment on Amazon Linux 2! Tanishq here New Version V2 v3 V4!V5! V6 v7 V8 V9 V10 v11 v12 v13"
+    return "Hello, Blue-Green Deployment on Amazon Linux 2! Tanishq here New Version V2 v3 V4!V5! V6 v7 V8 V9 V10 v11 v12 v13 v14"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
