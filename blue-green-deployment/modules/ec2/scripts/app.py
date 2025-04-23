146427bfa2b3aadebb8e8af59fa4e25f4b8612d9
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-04-23 20:19:25 +0530

    Update app.py

--- blue-green-deployment/modules/ec2/scripts/app.py
+++ blue-green-deployment/modules/ec2/scripts/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Blue-Green Deployment on Amazon Linux 2! Tanishq here v1 v2"
+    return "Hello, Blue-Green Deployment on Amazon Linux 2! Tanishq here v1 v2 v3"
 
 @app.route('/health')
 def health():
