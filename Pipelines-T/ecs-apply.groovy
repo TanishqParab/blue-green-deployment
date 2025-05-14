acccc9ca84e8684a6c0ddc8e590667ef46d1babe
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-05-14 19:29:18 +0530

    Update ecs-apply.groovy

--- Pipelines-T/ecs-apply.groovy
+++ Pipelines-T/ecs-apply.groovy
@@ -4,7 +4,7 @@
 
 // Call the ECS pipeline with custom configuration
 ecsPipeline([
-    tfWorkingDir: "/var/lib/jenkins/workspace/blue-green-deployment-job-ecs-test-apply/blue-green-deployment",
+    tfWorkingDir: "/var/lib/jenkins/workspace/blue-green-deployment-job/blue-green-deployment",
     emailRecipient: "tanishqparab2001@gmail.com",
     ecrRepoName: "blue-green-app",
     varFile: "terraform.tfvars"
