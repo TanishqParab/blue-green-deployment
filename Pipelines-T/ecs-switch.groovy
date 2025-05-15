72a13bd2d02678bd583b929e702b5e2e3d760a79
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-05-15 12:17:25 +0530

    Update ecs-switch.groovy

--- Pipelines-T/ecs-switch.groovy
+++ Pipelines-T/ecs-switch.groovy
@@ -4,7 +4,7 @@
 
 // Call the ECS switch pipeline with custom configuration
 ecsSwitchPipeline([
-    tfWorkingDir: "/var/lib/jenkins/workspace/blue-green-deployment-job/blue-green-deployment",
+    tfWorkingDir: "/var/lib/jenkins/workspace/blue-green-deployment-job-ecs-switch-test/blue-green-deployment",
     emailRecipient: "tanishqparab2001@gmail.com",
     ecrRepoName: "blue-green-app"
 ])
