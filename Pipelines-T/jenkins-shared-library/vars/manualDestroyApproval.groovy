ee56e0a13277280b9c470760455c4087d2d362ae
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-05-19 14:22:30 +0530

    Update manualDestroyApproval.groovy

--- Pipelines-T/jenkins-shared-library/vars/manualDestroyApproval.groovy
+++ Pipelines-T/jenkins-shared-library/vars/manualDestroyApproval.groovy
@@ -1,29 +1,22 @@
-    
-def call(config) {    
-    stage('Manual Approval for Destroy') {
-        when {
-            expression { params.MANUAL_BUILD == 'DESTROY' }
-        }
-        steps {
-            script {
-                def destroyLink = "${env.BUILD_URL}input"
-                emailext (
-                    to: config.emailRecipient,
-                    subject: "🚨 Approval Required for Terraform Destroy - Build ${currentBuild.number}",
-                    body: """
-                    WARNING: You are about to destroy AWS infrastructure.
+// vars/manualDestroyApproval.groovy
 
-                    👉 Click the link below to approve destruction:
+def call(config) {
+    def destroyLink = "${env.BUILD_URL}input"
 
-                    ${destroyLink}
-                    """,
-                    replyTo: config.emailRecipient
-                )
+    emailext (
+        to: config.emailRecipient,
+        subject: "🚨 Approval Required for Terraform Destroy - Build ${currentBuild.number}",
+        body: """
+        WARNING: You are about to destroy AWS infrastructure.
 
-                timeout(time: 1, unit: 'HOURS') {
-                    input message: '⚠️ Confirm destruction of infrastructure?', ok: 'Destroy Now'
-                }
-            }
-        }
+        👉 Click the link below to approve destruction:
+
+        ${destroyLink}
+        """,
+        replyTo: config.emailRecipient
+    )
+
+    timeout(time: 1, unit: 'HOURS') {
+        input message: '⚠️ Confirm destruction of infrastructure?', ok: 'Destroy Now'
     }
 }
