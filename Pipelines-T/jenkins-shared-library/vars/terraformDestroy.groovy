77bae41e32577792cba116a92b6d5658d81a9b28
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-05-19 13:30:53 +0530

    Update terraformDestroy.groovy

--- Pipelines-T/jenkins-shared-library/vars/terraformDestroy.groovy
+++ Pipelines-T/jenkins-shared-library/vars/terraformDestroy.groovy
@@ -1,27 +1,35 @@
 // vars/terraformDestroy.groovy
 
-def call(Map config) {
-    def varFileOption = config.varFile ? "-var-file=${config.varFile}" : ""
-    
-    // Get the build number containing the state
-    def buildNumber = input(
-        message: "Enter the build number that created the infrastructure (e.g., 42)",
-        parameters: [string(name: 'BUILD_NUMBER')]
-    )
+def call(config) {
+    stage('Destroy Infrastructure') {
+        when {
+            expression { params.MANUAL_BUILD == 'DESTROY' }
+        }
+        steps {
+            script {
+                def buildNumber = input(
+                    message: "Enter the build number that created the infrastructure (e.g., 42)",
+                    parameters: [string(name: 'BUILD_NUMBER')]
+                )
 
-    // Fetch the archived state file
-    dir(config.tfWorkingDir) {
-        copyArtifacts(
-            projectName: env.JOB_NAME,
-            selector: specific("${buildNumber}"),
-            filter: "terraform.tfstate",
-            target: "."
-        )
-    }
+                dir("${config.tfWorkingDir}") {
+                    copyArtifacts(
+                        projectName: env.JOB_NAME,
+                        selector: specific("${buildNumber}"),
+                        filter: "terraform.tfstate",
+                        target: "."
+                    )
+                }
 
-    // Initialize and destroy
-    dir(config.tfWorkingDir) {
-        sh "terraform init"
-        sh "terraform destroy ${varFileOption} -auto-approve"
+                dir("${config.tfWorkingDir}") {
+                    sh "terraform init"
+                    def destroyCmd = "terraform destroy -auto-approve"
+                    if (config.varFile) {
+                        destroyCmd += " -var-file=${config.varFile}"
+                    }
+                    sh destroyCmd
+                }
+            }
+        }
     }
 }
