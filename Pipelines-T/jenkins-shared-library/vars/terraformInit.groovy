14f3246578f0a2dd0ec7225e6a2144e807abf7a0
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-05-19 14:10:34 +0530

    Update terraformInit.groovy

--- Pipelines-T/jenkins-shared-library/vars/terraformInit.groovy
+++ Pipelines-T/jenkins-shared-library/vars/terraformInit.groovy
@@ -1,17 +1,8 @@
 // vars/terraformInit.groovy
 
 def call(config) {
-    stage('Terraform Init') {
-        when {
-            expression { env.EXECUTION_TYPE == 'FULL_DEPLOY' || env.EXECUTION_TYPE == 'MANUAL_APPLY' }
-        }
-        steps {
-            script {
-                echo "Initializing Terraform..."
-                dir("${config.tfWorkingDir}") {
-                    sh "terraform init"
-                }
-            }
-        }
+    echo "Initializing Terraform..."
+    dir("${config.tfWorkingDir}") {
+        sh "terraform init"
     }
 }
