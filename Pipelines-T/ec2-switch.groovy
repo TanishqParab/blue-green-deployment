9a8d4ab34411f2b3441d90055d7fe1f76a2202e1
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-05-22 16:25:34 +0530

    Update ec2-switch.groovy

--- Pipelines-T/ec2-switch.groovy
+++ Pipelines-T/ec2-switch.groovy
@@ -4,7 +4,7 @@
 
 // Call the EC2 switch pipeline with custom configuration
 ec2SwitchPipeline([
-    tfWorkingDir: "/var/lib/jenkins/workspace/blue-green-deployment-job-ec2-switch-test/blue-green-deployment",
+    tfWorkingDir: "/var/lib/jenkins/workspace/blue-green-deployment-job-ec2-switch-test/blue-green-deployment-ec2",
     emailRecipient: "tanishqparab2001@gmail.com",
     sshKeyId: "blue-green-key"
 ])
