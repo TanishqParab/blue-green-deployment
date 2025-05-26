1923dcd635cfcb7d86fbdf2764955cc8dbd09e5f
TanishqParab <130289493+TanishqParab@users.noreply.github.com>
2025-05-26 14:54:03 +0530

    Update ec2-apply.groovy

--- Pipelines-T/ec2-apply.groovy
+++ Pipelines-T/ec2-apply.groovy
@@ -4,7 +4,7 @@
 
 // Call the EC2 pipeline with custom configuration
 ec2Pipeline([
-    tfWorkingDir: "/var/lib/jenkins/workspace/blue-green-deployment-job-apply/blue-green-deployment-ec2",
+    tfWorkingDir: "/var/lib/jenkins/workspace/blue-green-deployment-job-apply/blue-green-deployment",
     emailRecipient: "tanishqparab2001@gmail.com",
     sshKeyId: "blue-green-key",
     varFile: "terraform.tfvars"
